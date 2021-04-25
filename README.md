40bed1cced539969c41cb785f5f87d2db324d614
Lucas Capuano <lucasramoscapuano@gmail.com>
2021-04-25 14:42:16 -0300

    cria o projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# ContaBancaria
+Projeto acadêmico que utiliza os conceitos do curso orienteção a objetos em Java da **Alura**.
+ 
---
+++ src/TestaReferencia.java
@@ -0,0 +1,23 @@
+
+public class TestaReferencia {
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("saldo da primeira: " + primeiraConta.saldo);
+		
+		Conta  segundaConta= primeiraConta;
+		System.out.println("saldo da segunda.conta: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("saldo da segunda.conta: " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+				System.out.println("sao a mesmissima conta");
+		}
+	}	
+
+}
